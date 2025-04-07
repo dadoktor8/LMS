5f796281f279c77c134734229a5d4543b330eecb
dadoktor8 <dadoktor8@gmail.com>
2025-04-07 09:14:50 +0530

    Made the login and signup page beautiful

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,5 +1,5 @@
 # backend/db/models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from db.database import Base
 
 class User(Base):
@@ -7,5 +7,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    is_verified = Column(Boolean, default=False)
     hashed_password = Column(String, nullable=False)
     role = Column(String, nullable=False)
